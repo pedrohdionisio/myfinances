184e8eaf37b3a678c5bfb2d925cc91e9212e8d34
Pedro Henrique Dionisio <pedrohenriquedionisioalves@gmail.com>
2021-11-30 00:24:50 -0300

    refactor: change main container max-width

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   width: 100%;
-  max-width: 500px;
+  max-width: 700px;
   margin: 0 auto;
   margin-bottom: 80px;
   padding: 0 16px;
